f007db8b4bb738768856ed2cfdb281fda4559dbf
Leo Roberts <100042123+LeoRoberts21@users.noreply.github.com>
2022-07-08 01:04:50 +0100

    Changed images before hover animations

--- portfolio.js
+++ portfolio.js
@@ -8,19 +8,19 @@ let day = String(today.getDay());
 
 console.log(day);
 
-if (day === 0) {
+if (day == 0) {
   document.querySelector('.current-day').textContent = 'Sunday';
-} else if (day === 1) {
+} else if (day == 1) {
   document.querySelector('.current-day').textContent = 'Monday';
-} else if (day === 2) {
+} else if (day == 2) {
   document.querySelector('.current-day').textContent = 'Tuesday';
-} else if (day === 3) {
+} else if (day == 3) {
   document.querySelector('.current-day').textContent = 'Wednesday';
 } else if (day == 4) {
   document.querySelector('.current-day').textContent = 'Thursday';
-} else if (day === 5) {
+} else if (day == 5) {
   document.querySelector('.current-day').textContent = 'Friday';
-} else if (day === 6) {
+} else if (day == 6) {
   document.querySelector('.current-day').textContent = 'Saturday';
 }
 
@@ -32,11 +32,17 @@ let lastScrollY = window.scrollY;
 
 window.addEventListener("scroll", () => {
   if(lastScrollY < window.scrollY && lastScrollY > 0.1) {
-    nav.classList.add("nav--hidden");
+    nav.classList.add("nav--hidden", "navbar2");
   }else {
     nav.classList.remove("nav--hidden");
-  }
-  
+  };
+
   lastScrollY = window.scrollY;
+
+  if(window.scrollY < 0.5) {
+    nav.classList.remove("navbar2");
+  }
+
+
 
 });
