add1da82797c1078c7f1e73ac73b0c02644ee4a5
Leo Roberts <100042123+LeoRoberts21@users.noreply.github.com>
2022-07-18 22:52:06 +0100

    responsive start

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 // DAY READING
 
 let today = new Date();
@@ -9,43 +8,44 @@ let day = String(today.getDay());
 console.log(day);
 
 if (day == 0) {
-  document.querySelector('.current-day').textContent = 'Sunday';
+  document.querySelector(".current-day").textContent = "Sunday";
 } else if (day == 1) {
-  document.querySelector('.current-day').textContent = 'Monday';
+  document.querySelector(".current-day").textContent = "Monday";
 } else if (day == 2) {
-  document.querySelector('.current-day').textContent = 'Tuesday';
+  document.querySelector(".current-day").textContent = "Tuesday";
 } else if (day == 3) {
-  document.querySelector('.current-day').textContent = 'Wednesday';
+  document.querySelector(".current-day").textContent = "Wednesday";
 } else if (day == 4) {
-  document.querySelector('.current-day').textContent = 'Thursday';
+  document.querySelector(".current-day").textContent = "Thursday";
 } else if (day == 5) {
-  document.querySelector('.current-day').textContent = 'Friday';
+  document.querySelector(".current-day").textContent = "Friday";
 } else if (day == 6) {
-  document.querySelector('.current-day').textContent = 'Saturday';
+  document.querySelector(".current-day").textContent = "Saturday";
 }
-
 
 // HIDDEN NAV-BAR
 
-const nav = document.querySelector('.nav-bar');
+const nav = document.querySelector(".nav-bar");
 let lastScrollY = window.scrollY;
 
 window.addEventListener("scroll", () => {
-  if(lastScrollY < window.scrollY && lastScrollY > 0.1) {
+  if (lastScrollY < window.scrollY && lastScrollY > 0.1) {
     nav.classList.add("nav--hidden", "navbar2");
-  }else {
+  } else {
     nav.classList.remove("nav--hidden");
-  };
+  }
 
   lastScrollY = window.scrollY;
 
-  if(window.scrollY < 0.5) {
+  if (window.scrollY < 0.5) {
     nav.classList.remove("navbar2");
   }
-
 });
 
 // FOR DARK MODE USE .TOGGLE
 
- 
-//PORTFOLIO
+let switchButton = document.querySelector(".switchButton");
+
+const toggle = function myFunction() {
+  switchButton.classList.add("switchButton");
+};
