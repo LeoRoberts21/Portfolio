bb020cac09c88be62d7cdfecd73ea58e2c4fbf0e
Leo Roberts <100042123+LeoRoberts21@users.noreply.github.com>
2022-08-10 21:57:07 +0100

    about page responsive(below 1350px)

--- about.js
+++ about.js
@@ -1,9 +1,11 @@
 "use strict";
 
-
 const nav = document.querySelector(".nav-bar");
 const leo = document.querySelector(".leo");
 const hamburger = document.querySelector(".hamburger");
+const white = document.querySelector(".white");
+const white2 = document.querySelector(".white2");
+const white3 = document.querySelector(".white3");
 let lastScrollY = window.scrollY;
 
 window.addEventListener("scroll", () => {
@@ -11,6 +13,9 @@ window.addEventListener("scroll", () => {
     nav.classList.add("nav--hidden", "navbar2");
     leo.classList.add("leo2");
     hamburger.classList.add("hamburger2");
+    white.classList.add("nowhite");
+    white2.classList.add("nowhite");
+    white3.classList.add("nowhite");
   } else {
     nav.classList.remove("nav--hidden");
   }
@@ -21,5 +26,8 @@ window.addEventListener("scroll", () => {
     nav.classList.remove("navbar2");
     hamburger.classList.remove("hamburger2");
     leo.classList.remove("leo2");
+    white.classList.remove("nowhite");
+    white2.classList.remove("nowhite");
+    white3.classList.remove("nowhite");
   }
 });
